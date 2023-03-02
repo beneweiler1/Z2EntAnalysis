ce4fa061b17dc72b7ddd8c10cc7fdbd5e35e3f53
beneweiler1 <beneweiler1@gmail.com>
2023-03-01 22:02:11 -0700

    reciever and server

---
+++ reciever/reciever.js
@@ -0,0 +1,155 @@
+const express = require('express');
+const axios = require("axios");
+const app = express();
+const port = 4200;
+const AWS = require('aws-sdk');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create a new DynamoDB client
+const dynamoDB = new AWS.DynamoDB.DocumentClient({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+// Start the Express application
+app.listen(port, () => {
+  console.log('Server listening on port 4200');
+  editData('7fM3iJ14tE5SX1L1TVpf7p', 'So Long - Tails Remix','2021-06-25',0, 1, 5, 456, 4, 456, 546, 45, 45, 5, 5);
+});
+
+// Define a route that queries the DynamoDB table
+app.get('/getItems', async (req, res) => {
+  try {
+    const result = await dynamoDB.scan({
+      TableName: 'SongData'
+    }).promise();
+    console.log(result.Items.length)
+    res.json(result.Items);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.get('/putItems/:id/:name', async (req, res) => {
+  const { id, name, popularity } = req.params;
+
+  if (!id || !name || !popularity) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  try {
+    const result = await dynamoDB.put({
+      TableName: 'SongData',
+      Item: { id, name, popularity }
+    }).promise();
+
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+async function addData(id, name, release_date, popularity){
+  try {
+    const result = await dynamoDB.put({
+      TableName: 'SongData',
+      Item: { id, name, release_date, popularity}
+    }).promise();
+
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function editData(id, name, release_date, popularity, danceability, energy, key, loudness, speechiness, acousticness, instramentalness, liveness, valence, tempo){
+  try {
+    const result = await dynamoDB.put({
+      TableName: 'SongData',
+      Item: {id, name, release_date, popularity, danceability, energy, key, loudness, speechiness, acousticness, instramentalness, liveness, valence, tempo}
+    }).promise();
+
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+app.get('/collectData', async (req, res) => {
+    try {
+      const tableSongs = await dynamoDB.scan({
+        TableName: 'SongData',
+      }).promise();
+      console.log(tableSongs.length)
+  
+      if (tableSongs.Count === 0) {
+        return res.status(404).json({ error: 'Item not found' });
+      }
+
+      const token = await axios.get(
+        "http://localhost:8080/getToken"
+      );
+
+      for(let index = 0; index < tableSongs.Items.length; index++){
+        currSongId = tableSongs.Items[index].id
+        const recData = await axios.get(
+          "http://localhost:8080/getSongRec/" + 1 + "/" + currSongId + "/" + token.data,
+        );
+        console.log(recData.data)
+        for(let size = 0; size < recData.data.length; size++){
+          addData(recData.data[size].id, recData.data[size].name, recData.data[size].release_date, recData.data[size].popularity);
+        }
+      }
+      res.send('done!')
+    
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+
+  app.get('/editData', async(req,res) => {
+    try {
+      const tableSongs = await dynamoDB.scan({
+        TableName: 'SongData',
+      }).promise();
+      console.log(tableSongs.length)
+  
+      if (tableSongs.Count === 0) {
+        return res.status(404).json({ error: 'Item not found' });
+      }
+
+      const token = await axios.get(
+        "http://localhost:8080/getToken"
+      );
+      
+      for(let itemIndex = 0; itemIndex < tableSongs.Items.length; itemIndex++){
+        let songData = await axios.get(
+          "http://localhost:8080/getSong/"+tableSongs.Items[itemIndex].id + "/" + token.data,
+        );
+        let s = songData.data
+        editData(tableSongs.Items[itemIndex].id, tableSongs.Items[itemIndex].name, tableSongs.Items[itemIndex].releasee_date, s.popularity, s.danceability, s.energy, s.key, s.loudness, s.speechiness, s.acousticness, s.instramentalness, s.liveness, s.valence, s.tempo)
+      }
+
+
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  })
+
+      //take playlist
+      //get songs name and id and store them into the db
+      //loop
+        //go through dynamo and do 2 things
+          //1. append the extra song data to the item in db
+          //2. get song rec based on song add them to db
---
+++ server/server.js
@@ -0,0 +1,201 @@
+//const express = require("express");
+import axios from "axios";
+import express from 'express';
+import queryString from 'query-string';
+import * as dotenv from 'dotenv'
+const app = express();
+const port = 8080;
+dotenv.config()
+let token = ""
+//https://medium.com/@awoldt/using-spotify-api-with-javascript-9dd839407f12
+//https://developer.spotify.com/console/get-search-item/?q=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=&market=ES&limit=10&offset=5&include_external=
+
+
+function getCookie(cookie) {
+  const parts = cookie.split(':');
+  console.log(parts[1])
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+app.listen(8080, () => {
+  console.log("App is listening on port 8080!\n");
+});
+
+app.get("/", (req, res) => {
+    res.send(
+      "<a href='https://accounts.spotify.com/authorize?client_id=" + process.env.CLIENT_ID +
+       "&response_type=code&redirect_uri=" + process.env.URI_ENCODED + "&scope=user-top-read'>Sign in</a>"
+    );
+  }) 
+  
+
+  app.get("/account", async (req, res) => {
+    //console.log("spotify response code is " + req.query.code);
+    const spotifyResponse = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        queryString.stringify({
+          grant_type: "authorization_code",
+          code: req.query.code,
+          redirect_uri: process.env.URI_DECODED,
+        }),
+        {
+          headers: {
+            Authorization: "Basic " + process.env.AUTH_ENCODED,
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      token = String(spotifyResponse.data.access_token);
+      //console.log(token)
+      //res.send(token)
+      res.redirect('/data');
+      })
+
+
+      app.get('/getToken', async (req, res) => {
+        console.log(token)
+        res.send(token);
+      })
+
+      app.get("/data/:artist", async (req, res) => {
+        const data = await axios.get(
+          "https://api.spotify.com/v1/search?q=" + req.params.artist +"&type=artist&limit=5",
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+        let artistId = data.data.artists.items[0].id
+        const artistInfo = await axios.get(
+          "https://api.spotify.com/v1/artists/"+artistId+"/related-artists?limit=5",
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+        //console.log(artistInfo.data.artists)
+
+        res.send(artistInfo.data.artists[0])
+        //res.redirect(artistInfo.data.artists[0].external_urls.spotify)
+      }) 
+
+     app.get("/playlist/:id", async (req, res) => {
+      try{
+        const playlistData = await axios.get(
+          "https://api.spotify.com/v1/playlists/" + req.params.id + "/tracks",
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+        const songs = []
+        let song = {}
+        for(const items of playlistData.data.items){
+          song = {name:items.track.name, id: items.track.id, releaseDate:items.track.album.release_date}
+          songs.push(song)
+        }
+        console.log(songs)
+        res.send(songs)
+      }
+      catch(error){
+        res.status(500).send(error);
+      }
+    })
+
+    app.get('/getSong/:id/:token', async (req, res) => {
+
+        const songData = await axios.get(
+          "https://api.spotify.com/v1/audio-features/" + req.params.id,
+          {
+            headers: {
+              Authorization: "Bearer " + req.params.token,
+            },
+          }
+        );
+        //dancability
+        //engery
+        //key
+        //loudness
+        //speechiness
+        //acousticness
+        //instramentalness
+        //liveness
+        //valence
+        //tempo
+        const sData = songData.data
+        let song = {danceability:sData.danceability,
+          energy:sData.energy,
+          key:sData.key,
+          loudness:sData.loudness,
+          speechiness:sData.speechiness,
+          acousticness: sData.acousticness,
+          instramentalness:sData.instramentalness,
+          liveness: sData.liveness,
+          valence: sData.valence,
+          tempo: sData.tempo
+        }
+        console.log(song);
+        res.send(song);
+    })
+
+    app.get("/getPlaylistRec/:limit/:genre/:minpop/:maxpop", async (req, res) => {
+        //token ="BQCD4OATmg3W44peHS07Z6YpgJOdfdNqiabXz82MGZDqQsziO3EWhCu-UAXZI5zQ4KcFebLzvXGAPr-43UjAbTBhMv4cLveNMk52-4P4zg87HGVvhSsj8Hm7LBnZ3ScN0Q6IgLVhVR4pqIUgQ66xRFt6dPY43-4pu1zRT42ILpYmwNiIFJfcYatB54Zxmp44qqIIchTPUAk"
+        try{
+          const recData = await axios.get(
+            //"https://api.spotify.com/v1/recommendations?limit=100&seed_genres=country&min_popularity=1&max_popularity=100",
+            "https://api.spotify.com/v1/recommendations?limit=" + req.params.limit + "&seed_genres=" + req.params.genre + "&min_popularity=" + req.params.minpop + "&max_popularity=" + req.params.maxpop,
+            {
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            }
+          );
+          let songRecData = []
+          let songRec = {}
+          for(const tracks of recData.data.tracks){
+            songRec = {'name':tracks.name, 'id':tracks.id, 'popularity':tracks.popularity, 'release_date': items.track.album.release_date}
+            console.log(songRec)
+            songRecData.push(songRec)
+
+          }
+          res.send(songRecData)
+        }
+        catch(error){
+          res.status(500).send(error);
+        }
+      })
+
+      app.get("/getSongRec/:limit/:songid/:token", async (req, res) => {
+        //app.get("/getSongRec/:limit/:songid", async (req, res) => {
+        try{
+          const recData = await axios.get(
+            "https://api.spotify.com/v1/recommendations?limit=" + req.params.limit + "&seed_tracks="+req.params.songid,
+            {
+              headers: {
+                Authorization: "Bearer " + req.params.token,
+              },
+            }
+          );
+          let songRecData = []
+          let songRec = {}
+          for(const tracks of recData.data.tracks){
+            console.log(tracks.album.release_date)
+            songRec = {'name':tracks.name, 'id':tracks.id, 'popularity':tracks.popularity,'release_date':tracks.album.release_date}
+            songRecData.push(songRec)
+
+          }
+          res.send(songRecData)
+        }
+        catch(error){
+          res.status(500).send(error);
+        }
+      })
+
+      
+      //take playlist
+      //get songs name and id and store them in json
+      //input take a song on playlist and query recommendations
+      //add track name and id to json
